cec93751f89e85338d7c50161d68e2fabf329e75
“SergeyPinkevich” <sergeipinkevich163@gmail.com>
2017-05-18 19:03:02 +0300

    commit for merge

---
+++ app/src/main/java/tests/velka/rxjavatest/presenters/MenuPresenter.java
@@ -0,0 +1,9 @@
+package tests.velka.rxjavatest.presenters;
+
+/**
+ * Created by Сергей Пинкевич on 10.05.2017.
+ */
+
+public class MenuPresenter {
+
+}
---
+++ app/src/main/java/tests/velka/rxjavatest/views/MainMenuView.java
@@ -0,0 +1,8 @@
+package tests.velka.rxjavatest.views;
+
+/**
+ * Created by Сергей Пинкевич on 10.05.2017.
+ */
+
+public interface MainMenuView {
+}
